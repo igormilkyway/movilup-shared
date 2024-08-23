6b6b999b51774b578f5248fae2b0c4463f836c04
Igor Sorokin <igormilkyway1@gmail.com>
2024-08-23 07:11:24 +0300

    Auth flow 8

--- Sources/MovilupShared/User/UserDTO.swift
+++ Sources/MovilupShared/User/UserDTO.swift
@@ -9,7 +9,7 @@ public struct UserDTO: Codable {
   public let firstName: String
   public let lastName: String?
   public let phoneNumber: String?
-  public let imageURL: URL?
+  public let imageURL: String?
   public let role: UserRole?
 
   public init(id: UUID?, email: String?, firstName: String, lastName: String?, phoneNumber: String?, imageURL: String?, role: UserRole?) {
@@ -19,24 +19,6 @@ public struct UserDTO: Codable {
     self.lastName = lastName
     self.phoneNumber = phoneNumber
     self.role = role
-
-    if
-      let imageURL,
-      let imageURL = URL(string: imageURL) {
-      self.imageURL = imageURL
-    } else {
-      self.imageURL = nil
-    }
-  }
-
-  public static func initForAnother(firstName: String,
-                                    imageURL: String?) -> UserDTO {
-    .init(id: nil,
-          email: nil,
-          firstName: firstName,
-          lastName: nil,
-          phoneNumber: nil,
-          imageURL: imageURL,
-          role: nil)
+    self.imageURL = imageURL
   }
 }
