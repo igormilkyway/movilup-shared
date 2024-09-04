0102f5d6bd56ed57d46c7914f7825d780ce9b68b
Igor Sorokin <igormilkyway1@gmail.com>
2024-09-04 08:07:16 +0300

    Refactor OrderState

--- Sources/MovilupShared/Order/OrderState.swift
+++ Sources/MovilupShared/Order/OrderState.swift
@@ -11,3 +11,51 @@ public enum OrderState: String, Codable, Sendable {
   case arrivedAtDestination
   case closed
 }
+
+extension OrderState {
+  public var intValue: Int {
+    switch self {
+      case .created:
+        0
+      case .takenByEstimator:
+        1
+      case .estimated:
+        2
+      case .approved:
+        3
+      case .takenByDriver:
+        4
+      case .arrivedAtDeparture:
+        5
+      case .loaded:
+        6
+      case .arrivedAtDestination:
+        7
+      case .closed:
+        8
+    }
+  }
+
+  public init?(intValue: Int) {
+    switch intValue {
+      case 0:
+        self = .created
+      case 1:
+        self = .estimated
+      case 2:
+        self = .approved
+      case 3:
+        self = .takenByDriver
+      case 4:
+        self = .arrivedAtDeparture
+      case 5:
+        self = .loaded
+      case 6:
+        self = .arrivedAtDestination
+      case 7:
+        self = .closed
+      default:
+        return nil
+    }
+  }
+}
